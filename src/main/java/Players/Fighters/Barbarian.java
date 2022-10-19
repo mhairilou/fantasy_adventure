a6ba6dfc1cb1ae06522c1ad6d8730d526e3bf27d
Mhairi MacDonald <mhairilouyoga@gmail.com>
2022-10-19 13:20:50 +0100

    barbarian is clumsy

--- src/main/java/Players/Fighters/Barbarian.java
+++ src/main/java/Players/Fighters/Barbarian.java
@@ -1,5 +1,6 @@
 package Players.Fighters;
 
+import Abilities.ITakeDamage;
 import Weapons.AbstractWeapon;
 
 public class Barbarian extends AbstractFighter{
@@ -12,6 +13,16 @@ public class Barbarian extends AbstractFighter{
 
     @Override
     public void takeDamage(int amount) {
+        int hpAfterDamage = this.getHp() - amount;
+        if(hpAfterDamage < 0){ hpAfterDamage = 0;}
+        this.setHp(hpAfterDamage);
+    }
 
+
+    public String useWeapon(ITakeDamage opponent){
+        this.getCurrentWeapon().causeDamage(opponent);
+        int reboundDamage = getCurrentWeapon().getPower() / 2;
+        this.takeDamage(reboundDamage);
+        return "AAAAGHGHG!";
     }
 }
--- src/test/java/BarbarianTest.java
+++ src/test/java/BarbarianTest.java
@@ -1,5 +1,7 @@
 import Players.Fighters.Barbarian;
+import Players.Fighters.Knight;
 import Weapons.Club;
+import Weapons.Sword;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,17 +10,45 @@ import static org.junit.Assert.assertEquals;
 public class BarbarianTest {
 
     Barbarian barbarian;
+    Knight knight;
+    Sword sword;
     Club club;
 
     @Before
     public void before(){
         club = new Club(40);
         barbarian = new Barbarian("Woody", 300, club);
+        sword = new Sword(20, 100);
+        knight = new Knight("Cat", 200, sword);
     }
 
     @Test
     public void hasName(){
         assertEquals("Woody", barbarian.getName());
+    }
+
+    @Test
+    public void hasHp(){
+        assertEquals(300, barbarian.getHp());
+    }
+
+    @Test
+    public void hasWeapon(){
+        assertEquals(40, barbarian.getCurrentWeapon().getPower());
+    }
+
+    @Test
+    public void canUseWeapon(){
+        barbarian.useWeapon(knight);
+        assertEquals(160, knight.getHp());
+        String barbarianWail = barbarian.useWeapon(knight);
+        assertEquals("AAAAGHGHG!", barbarianWail);
+    }
+
+    @Test
+    public void barbarianIsClumsy(){
+        barbarian.useWeapon(knight);
+        assertEquals(280, barbarian.getHp());
     }
 
 }
